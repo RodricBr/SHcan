51aa9a21153d68e3712ac90296aa65fff71282be
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-07 20:38:51 -0300

    Update shcanner.sh

--- shcanner.sh
+++ shcanner.sh
@@ -7,17 +7,17 @@ FIM='\033[m'
 
 if [[ -z "$@" ]]; then
   echo -e "\n${AMARELO}Como usar:${FIM}
-$0 <IP/Domnínio> <Range de portas> <-A (Agressivo) | -P (Passivo)>\n\
-Ex: $0 192.168.1.4 80-443\n\
-Identificações: Portas, serviços, sistemas operacionais
+\t$0 <IP/Domnínio> <Range de portas> <-A (Agressivo) | -P (Passivo)>\n\
+\tEx: $0 192.168.1.4 80-443\n\
+\tIdentificações: Portas, serviços, sistemas operacionais
   "
   if command -v nmap >/dev/null 2>&1; then
       echo -e "${VERDE}NMAP instalado!${FIM}"
-      echo "Versão: $(nmap -v)"
+      echo -e "Versão: $(nmap -v)"
       exit 0
   else
       echo -e "${VERMELHO}NMAP não encontrado!${FIM}"
-      echo -e "Para baixar, execute esse comando:\n"
+      echo -e "Para baixar, execute esse comando:"
       echo -e "sudo apt-get install nmap\n"
       exit 1
   fi
